de4764c162ae31184d9385f31e22908120fbf737
Muhammad Usama <usama@192.168.1.13>
2024-03-21 12:05:04 +0500

    setting up the Server with MongoDB and Redux Tool Kit

---
+++ client/src/Users.jsx
@@ -0,0 +1,24 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Users() {
+  return (
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+      <div className="w-50 bg-white rounded p-3">
+        <button className="btn btn-success btn-sm">Add+</button>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Age</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default Users;
---
+++ client/src/redux/userSlice.jsx
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: [],
+  },
+  reducers: {},
+});
+
+export const {} = userSlice.actions;
+export default userSlice.reducer;
